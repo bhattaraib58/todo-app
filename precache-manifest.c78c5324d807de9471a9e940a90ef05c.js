d79302b356d397a688d44ad499affc4dfcfa5452
Biplap Bhattarai <bhattaraib58@gmail.com>
2019-07-22 17:53:16 +0545

    Updates

---
+++ precache-manifest.c78c5324d807de9471a9e940a90ef05c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca09cc959c0126842720512a0fa710b2",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "72594fc86479a36ed396",
+    "url": "/todo-app/static/css/main.ffbddcad.chunk.css"
+  },
+  {
+    "revision": "d121aa7f415e490f4eb3",
+    "url": "/todo-app/static/js/2.3fbd6399.chunk.js"
+  },
+  {
+    "revision": "72594fc86479a36ed396",
+    "url": "/todo-app/static/js/main.78ed67a4.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
